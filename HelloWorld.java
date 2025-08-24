7776b181fca26a5bf87c3cad00796135335ba17d
Scott Mann <s.mann@latrobe.edu.au>
2025-08-25 08:32:23 +1000

    Update for demo

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,6 +17,6 @@
  */
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello " + args[0]);
+        System.out.println("Hello Visitor " + args[0]);
     }
 }
